a8b75efe6f8b9ff31777995695612d3a676cd20a
zhangzongjian <1102349546@qq.com>
2023-03-28 02:05:21 +0800

    【zutil.sh】

--- zutil.sh
+++ zutil.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+zutil=$(cat /root/.bashrc | grep "zutil-add" | grep -Eo "source .*.sh" | awk '{print $2}')
+
 function zupdate() {
-  local zutil=/tmp/zzj/zutil.sh
+  [ -z "${zutil}" ] && zutil=/tmp/zzj/zutil.sh
   wget http://192.168.1.5:8008/D%3A/IdeaProjects/Markdown/zutil.sh -O $zutil.tmp
   if [ $(wc -l < $zutil.tmp) -ne 0 ]; then
     set -x
@@ -11,8 +13,29 @@ function zupdate() {
   source ${zutil}
 }
 
-function zhelp() {
+function zdownload() {
   date
+}
+
+function zhelp() {
+  local fun_name
+  local remark
+  local blank_str="                                "
+  grep "^function " ${zutil} | while read -r fun_name; do
+    remark=""
+    local line_num=$(grep -n "^${fun_name}$" ${zutil} | awk -F':' '{print $1}')
+    for ((line_num = line_num - 1; line_num > 0; line_num--)); do
+      local line_str=$(sed -n "${line_num} p" ${zutil})
+      if [ $(expr index "${line_str}" "#") -eq 1 ]; then
+        remark=$(echo -e "${remark}\n${blank_str}${line_str}")
+      else
+        fun_name=$(echo ${fun_name} | awk '{print $2}' | sed 's/()//g')
+        echo ${fun_name}"$(echo "${remark}" | tac)" | sed "1 s/${blank_str}/${blank_str:${#fun_name}}/g" | sed "s/ #/ /g"
+        break
+      fi
+    done
+    echo "-------------------------------------"
+  done
 }
 
 function zfind() {
@@ -21,7 +44,8 @@ function zfind() {
     local grep_str=$2
     shift 2
     local cmd="find $(pwd) -name \"$name\" | xargs grep --color -n \"${grep_str}\" $@"
-    echo "$cmd"; eval "$cmd"
+    echo "$cmd"
+    eval "$cmd"
     return
   fi
   if [[ $# -gt 0 && $1 != -* ]]; then
@@ -42,7 +66,7 @@ function zfind() {
   find $(pwd) "$@"
 }
 
-function main() {
+function zmain() {
   if [[ "$0" == *zutil.sh ]]; then # sh zutil.sh
     local zutil=$(readlink -m $0)
     local profile=/root/.bashrc
@@ -61,4 +85,4 @@ function main() {
   fi
 }
 
-main "$@"
+zmain "$@"
